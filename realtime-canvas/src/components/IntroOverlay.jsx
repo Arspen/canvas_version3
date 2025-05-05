d79f41df5175ecda0ec5a24e0b41e5ab102ddcad
Arspen <70844514+Arspen@users.noreply.github.com>
2025-05-05 18:05:19 +0200

    timer changes

--- realtime-canvas/src/components/IntroOverlay.jsx
+++ realtime-canvas/src/components/IntroOverlay.jsx
@@ -9,7 +9,7 @@ import './introOverlay.css';
 export default function IntroOverlay({ onDone }) {
   /* auto-dismiss */
   useEffect(() => {
-    const id = setTimeout(onDone, 20000);
+    const id = setTimeout(onDone, 4000);
     return () => clearTimeout(id);
   }, [onDone]);
 
