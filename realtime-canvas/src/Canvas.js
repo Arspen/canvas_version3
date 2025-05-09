6bfcf3b6679f98baa8472536c316b0f055ac13b4
Arspen <70844514+Arspen@users.noreply.github.com>
2025-05-09 13:10:54 +0200

    Update Canvas.js

--- realtime-canvas/src/Canvas.js
+++ realtime-canvas/src/Canvas.js
@@ -189,7 +189,7 @@ export default function Canvas({ userId }) {
         : { width: window.innerWidth, height: window.innerHeight };
   
     const x = scrollX + rect.width  / 2;
-    const y = scrollY + rect.height / 2;
+    const y = scrollY + rect.height / 2 + 20;
   
     const emoji = getEmojiForWord(word);
   
